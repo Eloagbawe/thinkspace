8ba6b495cd7000844fc66e2f0d252550b42a72ee
Eloagbawe <eloagbawe@gmail.com>
2023-12-10 23:27:51 +0100

    remove imageUpload.ts

--- backend/src/utils/imageUpload.ts
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-const memoryStorage = multer.memoryStorage();
-
-export const upload = multer({
-  storage: memoryStorage,
-  limits: { fileSize: 1 * 1024 * 1024 },
-}).any();
