62346fcf62c53a687c8dc14b35cf18aa3d0c5e10
Eloagbawe <eloagbawe@gmail.com>
2023-12-17 22:34:08 +0100

    add ngrx store devtools

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './store/reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideStore(reducers, {
-    metaReducers
-  })]
+        metaReducers
+    }), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
