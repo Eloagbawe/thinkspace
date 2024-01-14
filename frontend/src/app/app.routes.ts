6cb2617b265ce2d67bc72ac46d10d3c670f135a7
Eloagbawe <eloagbawe@gmail.com>
2024-01-14 23:23:57 +0100

    add blog details page and update routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { BlogDetailsComponent } from './pages/blog-details/blog-details.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      {path: '', component: HomeComponent}
+      {path: '', component: HomeComponent},
+      {path: 'blogs/:id', component: BlogDetailsComponent}
     ]
   }
 ];
