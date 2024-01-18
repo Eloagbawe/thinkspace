957e7fb29ef0801047e70fbddf48303124beca76
Eloagbawe <eloagbawe@gmail.com>
2024-01-18 23:43:54 +0100

    update main layout

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../partials/header/header.component';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from '../../partials/nav/nav.component';
-
+import { SidebarComponent } from '../../partials/sidebar/sidebar.component';
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterModule, NavComponent],
+  imports: [HeaderComponent, RouterModule, NavComponent, SidebarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
