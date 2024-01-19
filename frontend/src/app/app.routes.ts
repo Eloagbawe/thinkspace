94deccb658abd3ecc2e63a9a6b90455ee05386c2
Eloagbawe <eloagbawe@gmail.com>
2024-01-19 23:22:30 +0100

    update nav component and app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { BlogDetailsComponent } from './pages/blog-details/blog-details.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { AddBlogComponent } from './components/add-blog/add-blog.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      {path: '', component: HomeComponent},
-      {path: 'blogs/:id', component: BlogDetailsComponent}
+      {path: '', component: BlogsComponent},
+      {path: 'blogs/new', component: AddBlogComponent},
+      {path: 'blogs/:id', component: BlogComponent}
     ]
   }
 ];
