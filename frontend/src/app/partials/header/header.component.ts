c904902ee502b918a2ba6b1d8f83209c06b6978a
Eloagbawe <eloagbawe@gmail.com>
2024-01-25 23:15:55 -0500

    update header component

--- frontend/src/app/partials/header/header.component.ts
+++ frontend/src/app/partials/header/header.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { ProfileComponent } from '../../components/profile/profile.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, ProfileComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  display: boolean = false;
+
+  openModal = () => {
+    this.display = true;
+  }
+
+  closeModal = () => {
+    this.display = false;
+  }
 
 }
