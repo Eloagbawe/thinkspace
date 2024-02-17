99b3ef6db80b62f492fb683b639b72dff2254789
Eloagbawe <eloagbawe@gmail.com>
2024-02-16 23:13:06 -0500

    add login and signup components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
 import { BlogComponent } from './components/blog/blog.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { AddBlogComponent } from './components/add-blog/add-blog.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
@@ -11,7 +13,9 @@ export const routes: Routes = [
     children: [
       {path: '', component: BlogsComponent},
       {path: 'blogs/new', component: AddBlogComponent},
-      {path: 'blogs/:id', component: BlogComponent}
+      {path: 'blogs/:id', component: BlogComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'signup', component: SignUpComponent}
     ]
   }
 ];
