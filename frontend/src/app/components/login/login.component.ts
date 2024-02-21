a2bbd2afab75f865ba86fabb4d60198a388dafa9
Eloagbawe <eloagbawe@gmail.com>
2024-02-20 23:25:26 -0500

    update login form

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup,
+  Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, CommonModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup = new FormGroup({
+    email : new FormControl('', [
+      Validators.required
+    ]),
+    password : new FormControl('', [
+      Validators.required
+    ]),
+  },)
+
+  onSubmit() {
+    
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+  
+
+
 
 }
