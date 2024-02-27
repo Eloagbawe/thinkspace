d1594738804ad7e1cba076cb9e1d769707112cfb
Eloagbawe <eloagbawe@gmail.com>
2024-02-26 23:16:39 -0500

    update header component

--- frontend/src/app/partials/header/header.component.ts
+++ frontend/src/app/partials/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { ProfileComponent } from '../../components/profile/profile.component';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavComponent, ProfileComponent, CommonModule],
+  imports: [NavComponent, ProfileComponent, CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
