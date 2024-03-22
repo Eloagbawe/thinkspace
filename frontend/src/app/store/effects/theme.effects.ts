f50b393c4b0d289a9720f1caa4c3b0416668b439
Eloagbawe <eloagbawe@gmail.com>
2024-03-21 23:53:26 -0400

    update theme effect

--- frontend/src/app/store/effects/theme.effects.ts
+++ frontend/src/app/store/effects/theme.effects.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ThemeService } from '../../services/theme/theme.service';
 import { addDarkTheme, addLightTheme, getTheme } from '../actions/theme.actions';
+import { catchError, exhaustMap, map, throwError } from 'rxjs';
 
 @Injectable()
 export class ThemeEffects {
@@ -12,6 +13,25 @@ export class ThemeEffects {
 
   addDarkTheme$ = createEffect(() => this.actions$.pipe(
     ofType(addDarkTheme),
-    
+    map(() => {
+      this.themeService.addDarkTheme();
+      return addDarkTheme({theme: 'dark'})
+    }),
   ))
+
+  addLightTheme$ = createEffect(() => this.actions$.pipe(
+    ofType(addLightTheme),
+    map(() => {
+      this.themeService.addLightTheme();
+      return addDarkTheme({theme: 'light'})
+    }),
+  ))
+  
+  getTheme$ = createEffect(() => this.actions$.pipe(
+    ofType(getTheme),
+    map(() => {
+      const theme = this.themeService.getTheme();
+      return getTheme({ theme });
+    })
+  ));
 }
