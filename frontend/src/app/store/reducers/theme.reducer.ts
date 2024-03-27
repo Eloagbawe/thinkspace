5d95d07be7bbc912cdc9f324e13ba72595d08a76
Eloagbawe <eloagbawe@gmail.com>
2024-03-26 22:56:01 -0400

    update theme reducer

--- frontend/src/app/store/reducers/theme.reducer.ts
+++ frontend/src/app/store/reducers/theme.reducer.ts
@@ -1,7 +1,25 @@
 import { createReducer, on } from "@ngrx/store";
-import { addDarkTheme, addLightTheme, getTheme } from "../actions/theme.actions";
+import { addDarkThemeSuccess, addLightThemeSuccess, getThemeSuccess } from "../actions/theme.actions";
 import { initialThemeState } from "../state/theme.state";
 
 export const ThemeReducer = createReducer(
   initialThemeState,
+  on(addDarkThemeSuccess, (state) => {
+    return {
+      ...state,
+      theme: 'light'
+    }
+  }),
+  on(addLightThemeSuccess, (state) => {
+    return {
+      ...state,
+      theme: 'dark'
+    }
+  }),
+  on(getThemeSuccess, (state, action) => {
+    return {
+      ...state,
+      theme: action.theme
+    }
+  })
 )
